c85ad2fc8a3e909f522d809d6307fd4c711f73ab
Alexander Farber <Alexander.Farber@gmail.com>
2016-10-11 17:27:21 +0200

    Add spring-boot-starter Maven package. Set main classes for 2 projects. Wrongly copied inbound source code to outbond project - fix that.

--- MqttOutbound/src/main/java/de/afarber/mqttoutbound/MqttJavaApplication.java
+++ MqttOutbound/src/main/java/de/afarber/mqttoutbound/MqttJavaApplication.java
@@ -1,55 +1,61 @@
 package de.afarber.mqttoutbound;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.MessageProducer;
-import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
-import org.springframework.messaging.Message;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
+@SpringBootApplication
+@IntegrationComponentScan
 public class MqttJavaApplication {
 
     public static void main(String[] args) {
-    	new SpringApplicationBuilder(MqttJavaApplication.class)
-    			.web(false)
-    			.run(args);
+        ConfigurableApplicationContext context =
+        		new SpringApplicationBuilder(MqttJavaApplication.class)
+        				.web(false)
+        				.run(args);
+        MyGateway gateway = context.getBean(MyGateway.class);
+        gateway.sendToMqtt("foo");
     }
 
     @Bean
-    public MessageChannel mqttInputChannel() {
-    	return new DirectChannel();
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setServerURIs("tcp://localhost:11883");
+        //factory.setServerURIs("tcp://host1:1883", "tcp://host2:1883");
+        //factory.setUserName("username");
+        //factory.setPassword("password");
+        return factory;
     }
 
     @Bean
-    public MessageProducer inbound() {
-    	MqttPahoMessageDrivenChannelAdapter adapter =
-    			new MqttPahoMessageDrivenChannelAdapter(
-                                "tcp://localhost:1883", 
-                                "testClient",
-                                "topic1", 
-                                "topic2"
-                        );
-    	adapter.setCompletionTimeout(5000);
-    	adapter.setConverter(new DefaultPahoMessageConverter());
-    	adapter.setQos(1);
-    	adapter.setOutputChannel(mqttInputChannel());
-    	return adapter;
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler =
+                       new MqttPahoMessageHandler("testClient", mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("testTopic");
+        return messageHandler;
     }
 
     @Bean
-    @ServiceActivator(inputChannel = "mqttInputChannel")
-    public MessageHandler handler() {
-    	return new MessageHandler() {
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
 
-    		@Override
-    		public void handleMessage(Message<?> message) throws MessagingException {
-    			System.out.println(message.getPayload());
-    		}
-    	};
+    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+    public interface MyGateway {
+
+        void sendToMqtt(String data);
+
     }
 }
